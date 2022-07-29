1ba25ea17864581f6ba222ee56f09182de4884da
Joaquin <joaquinvivo@gmail.com>
2022-07-29 19:06:47 -0300

    started color manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture_course/app/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
---
+++ lib/test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture_course/app/app.dart';
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  void updateAppState() {
+    MyApp.instance.appState = 10;
+  }
+
+  void getAppState() {
+    debugPrint(MyApp.instance.appState.toString()); // 10
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
